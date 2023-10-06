672b765074d9442ecff542426b8ba2a1b53ba283
arozhdan <halala30@gmail.com>
2023-10-06 10:17:36 +0200

    [feat] chat rename delete, typing

--- src/entities/Chat/ui/ChatWindow/ChatInput/ChatInput.tsx
+++ src/entities/Chat/ui/ChatWindow/ChatInput/ChatInput.tsx
@@ -1,7 +1,6 @@
-import { PaperAirplaneIcon } from "@heroicons/react/24/outline"
 import styles from "./ChatInput.module.css"
 import { useState } from "react"
-
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline"
 interface Props {
   onSubmit: (message: string) => void
 }
